82086befc03aa92b0337419ab99e9192e8ccd922
Toktar <ufazak@gmail.com>
2022-03-06 01:48:27 +0600

    подключаем сторонние модули и регистрируем наши компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CategoryComponent} from "./components/category/category.component";
+import {ProductComponent} from "./components/product/product.component";
+import {OrderComponent} from "./components/order/order.component";
+import {FormsModule} from "@angular/forms";
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    ProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
