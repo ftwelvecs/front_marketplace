178d4438d09ddf4f09b44481a4c950cb79b5d797
Toktar <ufazak@gmail.com>
2022-03-06 01:47:47 +0600

    создаем компоненты Product, Category и Order

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,26 @@
+import {Component, ElementRef, Input} from "@angular/core";
+import {Category} from "../../shared/category.interface";
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent {
+
+  categories: Array<Category> = [
+    {name: 'Техника', description: 'Бытова техника'},
+    {name: 'Химия', description: 'Химия'},
+    {name: 'Мебель', description: 'Офисная мебель'},
+    {name: 'Продукты', description: 'Продукты питания'}
+  ]
+
+  displayedColumns: string[] = ['name', 'description'];
+
+  constructor() {
+  }
+
+  save() {
+    console.log("method save called")
+  }
+}
